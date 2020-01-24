7f9be72ce071c997d9a96c9968a5755962fa98a5
Qian Zhao <34727794+zq00@users.noreply.github.com>
2020-01-23 21:52:39 -0800

    Update read_data.R

--- section-5/result/read_data.R
+++ section-5/result/read_data.R
@@ -1,6 +1,8 @@
 fileloc = "/section-5/output/"
 ending = c("_1_1.txt", "_1_2.txt", "_2.txt")
 names = c("param", "beta_est", "cov_est", "std_est", "dev","rho")
+# because size restriction to uploaded data, beta_est, cov_est, std_est are splitted into 10 files
+# you need to adjust the endings here to load them properly
 
 for(name in names){
   obj <- NULL
