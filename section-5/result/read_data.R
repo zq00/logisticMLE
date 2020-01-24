fd34ab1f78a8dfaa01a5946a4a5768a424a4c33b
Qian Zhao <34727794+zq00@users.noreply.github.com>
2020-01-23 21:17:20 -0800

    create section 5 read output data

---
+++ section-5/result/read_data.R
@@ -0,0 +1,20 @@
+fileloc = "/section-5/output/"
+ending = c("_1_1.txt", "_1_2.txt", "_2.txt")
+names = c("param", "beta_est", "cov_est", "std_est", "dev","rho")
+
+for(name in names){
+  obj <- NULL
+  
+  for(end in ending){
+    new_name = paste0(fileloc, name, end)
+    
+    new_obj <- as.matrix(read.table(new_name,quote="\"", comment.char=""))
+    obj <- rbind(obj, new_obj)
+    
+    assign(name, get("obj")) 
+  }
+}
+
+tau_hat_1 <- cov_est
+tau_hat_2 <- as.matrix(read.table(paste0(fileloc, "hat-tau-rho_1.txt"), quote="\"", comment.char=""))
+
